76ee78a0c25b381733fcc572672d53958b8e00b3
VyDee <vy.duong@cuw.edu>
2018-06-26 09:34:08 -0500

    Initial commit

---
+++ Open Pause Menu/Assets/Scripts/testing scripts/LightIntensity.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LightIntensity : MonoBehaviour
+{
+    public Light myLight;
+    private float myintensity;
+    //private float Swidth = Screen.width;
+    //private float Sheight = Screen.height;
+    public Slider lightSlider;
+
+    private void Awake()
+    {
+        /*myLight = GetComponent<Light>();
+        lightSlider = GetComponent<Slider>();*/
+        myintensity = lightSlider.value;
+        lightSlider.maxValue = 200f;
+
+
+    }
+
+    private void Update()
+    {
+        myintensity = lightSlider.value;
+        myLight.intensity = myintensity;
+        
+    }
+
+    /*private void OnGUI()
+    {
+        myintensity = GUI.HorizontalSlider
+            (new Rect(Swidth / 2f - 150f, Sheight / 1.1f, 300, 30),
+            myintensity, 0.0f, 10.0f);
+    }*/
+}
